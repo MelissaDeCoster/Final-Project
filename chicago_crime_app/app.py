35ce69c10211223dc895aadc4dba27d850e9769d
MelissaDeCoster <melissa.inez.phillips@gmail.com>
2019-07-20 15:17:18 -0500

    Adding tons of stuff

--- chicago_crime_app/app.py
+++ chicago_crime_app/app.py
@@ -2,43 +2,28 @@ import os
 from flask import Flask, render_template, jsonify, request, redirect, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
 import pandas as pd
+
 
 
 app = Flask(__name__)
     
 # ************** Database Setup ***************
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///PUTDATABASENAMEHERE.sqlite"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///static/db/simpledata.sqlite"
 
 # Attach db to Flask app so Flask handels db session managment and other good things
 db = SQLAlchemy(app)
-# db.engine
 
-path_for_this_file = os.path.dirname(__file__)
-
-# Load in your map csv data
-db_table_name = "mapdata"
-absolute_csv_path = os.path.join(path_for_this_file,"static","sourcedata","map_data.csv")
-df = pd.read_csv(absolute_csv_path)
-
-# Load in graph csv data
-db_table_name2 = "graphdata"
-absolute_csv_path2 = os.path.join(path_for_this_file,"static","sourcedata","graph_data.csv")
-df2 = pd.read_csv(absolute_csv_path2)
-
-
-print("\nClean and transform ....\n")
-# CLEAN
-# TRANSFORM
-print(df.head())
-print(df2.head())
-
-df.to_sql(db_table_name, con=db.engine, if_exists="replace", chunksize=20000)
-print("Map Data Loaded!")
-df2.to_sql(db_table_name2, con=db.engine, if_exists="replace", chunksize=20000)
-print("Graph Data Loaded!")
+data = {}
+e = db.get_engine()
+table_names = e.table_names()
+for name in table_names:
+	tbl = e.execute('SELECT * FROM {}'.format(name)).fetchall()
+	data[name] = pd.DataFrame(tbl)
+	
+# Futher down in code, to access data:
+data['graphdata']
 
 
 
@@ -54,35 +39,27 @@ def renderHome():
 # ************** API ENDPOINTS ****************
 # *********************************************
 
-@app.route("/api/cities")
-def lStationsJson():
-    df = pd.read_sql(f"""
-        select  * 
-        from    {db_table_name}
-        """, db.engine)
-    json_str = df.to_json(orient="records")
+@app.route("/api/graph/<crime>")
+def lStationsJson(crime):
+    tbl = data['graphdata']
+    results = tbl[tbl[3] == crime]
+    json_str = results.to_json(orient="records")
     return Response(response=json_str, status=200, mimetype='application/json')
 
 
-@app.route("/api/games")
-def lStationsJson2():
-    df2 = pd.read_sql(f"""
-        select  * 
-        from    {db_table_name2}
-        """, db.engine)
-    json_str = df2.to_json(orient="records")
-    return Response(response=json_str, status=200, mimetype='application/json')
+# @app.route("/api/graph")
+# def lStationsJson2():
+#     graph = pd.read_sql(f"""
+#         select  * 
+#         from    {graph_data}
+#         """, db.engine)
+#     json_str = df.to_json(orient="records")
+#     return Response(response=json_str, status=200, mimetype='application/json')
 
-@app.route("/api/summary")
-def lStationsJson3():
-    df3 = pd.read_sql(f"""
-        select  * 
-        from    {db_table_name3}
-        """, db.engine)
-    json_str = df3.to_json(orient="records")
-    return Response(response=json_str, status=200, mimetype='application/json')
+
+    return jsonify(data)
 
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- chicago_crime_app/static/js/staticgraph.js
+++ chicago_crime_app/static/js/staticgraph.js
@@ -1,38 +1,56 @@
-function optionChanged(crimeOption){
-    d3.json(`/api/temp_v_incidents/${crimeOption}`).then( (data) => {
-        var panel = d3.select("#graph-display");
 
-        console.log('Option selected: ' + crimeOption);
-        panel.html("");
-        panel.text(data.graph_type);
-        console.log('Here\'s the data: ' + data.data);
-    })
-}
 
-function buildCharts(crimeOption) {
-    d3.json(`/samples/${crimeOption}`).then((data) => {
-      const otu_ids = data.otu_ids;
-      const otu_labels = data.otu_labels;
-      const crimeOption_values = crimeOption.sample_values;
 
-    // Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-    var pieData = [
-        {
-          values: sample_values.slice(0, 10),
-          labels: otu_ids.slice(0, 10),
-          hovertext: otu_labels.slice(0, 10),
-          hoverinfo: "hovertext",
-          type: "pie"
+
+
+d3.select("#staticcrime").on("change", function(){
+  crime = d3.select("#staticcrime").node().value;
+  buildCharts(crime);
+});
+
+function buildCharts(crime) {
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var chartsURL = "/api/graph/" + crime;
+    // @TODO: Build a Bubble Chart using the sample data
+    d3.json(chartsURL, function(error, response) {
+
+      var crimecount = response.map(function(x) {
+        return x["1"];
+      });
+
+      var temperature = response.map(function(x) {
+        return x["2"];
+      });
+       
+      console.log(response);
+
+      var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        x: temperature,
+        y: crimecount,
+        line: {
+          color: "#17BECF"
         }
-      ];
-  
-      var pieLayout = {
-        margin: { t: 0, l: 0 }
       };
-  
-      Plotly.plot("pie", pieData, pieLayout);
-    });
-  }
-  
+
+      var data = [trace1];
+
+      var layout = {
+        title: `${crime} VS TEMPERATURE`,
+        // xaxis: {
+        //   type: "temp"
+        // },
+        // yaxis: {
+        //   autorange: true,
+        //   type: "linear"
+        // }
+      };
+      
+      Plotly.newPlot("plot", data, layout);
+
+  });
+};
+
+
+
